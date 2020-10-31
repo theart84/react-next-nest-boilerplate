e75871a02c042376069635a93db389812d296058
Dmitry Fuks <difuks@gmail.com>
2020-10-31 07:55:12 +0300

    feat: remove fix for build of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,20 +3,6 @@ const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx'],
   addons: ['@storybook/addon-docs'],
-  babel: async (options) => ({
-    ...options,
-    plugins: [
-      ...options.plugins,
-      /*
-        TODO this plugin need for production build mode
-        issue: https://github.com/storybookjs/storybook/issues/12952
-        after fix:
-        Remove plugin
-        Remove --no-dll in package.json
-       */
-      '@babel/plugin-transform-react-jsx'
-    ]
-  }),
   webpackFinal: (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin());
 
